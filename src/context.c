e2ea44b211d5a0b352afdbb974da82b759a0ed3d
Michal Vasko <mvasko@cesnet.cz>
2015-07-07 11:31:48 +0200

    context OPTIMIZE ly_get_module_names() optimized

--- src/context.c
+++ src/context.c
@@ -287,21 +287,18 @@ ly_ctx_get_module_names(struct ly_ctx *ctx)
 {
     int i;
     char **result = NULL;
-    unsigned int count = 0;
 
     if (!ctx) {
         ly_errno = LY_EINVAL;
         return NULL;
     }
 
+    result = malloc((ctx->models.used+1) * sizeof *result);
+
     for (i = 0; i < ctx->models.used; i++) {
-        ++count;
-        result = realloc(result, count * sizeof *result);
-        result[count-1] = strdup(ctx->models.list[i]->name);
+        result[i] = strdup(ctx->models.list[i]->name);
     }
-    ++count;
-    result = realloc(result, count * sizeof *result);
-    result[count-1] = NULL;
+    result[i] = NULL;
 
     return result;
 }
