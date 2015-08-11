5a10543d29e0c58144b12df484a879db40a27c17
Michal Vasko <mvasko@cesnet.cz>
2015-08-11 14:56:06 +0200

    parse DOC added license

--- src/parse.c
+++ src/parse.c
@@ -1,3 +1,24 @@
+/**
+ * @file parse.c
+ * @author Michal Vasko <mvasko@cesnet.cz>
+ * @brief parsing functions implementation
+ *
+ * Copyright (c) 2015 CESNET, z.s.p.o.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Company nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ */
+
 #include <ctype.h>
 #include <assert.h>
 #include <string.h>
