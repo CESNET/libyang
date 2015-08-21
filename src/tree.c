d83c03ac0ccd7cd9cafc5f2b0449540ffb81f775
Michal Vasko <mvasko@cesnet.cz>
2015-08-21 09:09:24 +0200

    schema tree BUGFIX imported module could have been already freed

--- src/tree.c
+++ src/tree.c
@@ -1566,7 +1566,7 @@ module_free_common(struct lys_module *module, int free_int_mods)
         /* do not free internal modules */
         if (!free_int_mods) {
             for (j = 0; j < int_mods.count; ++j) {
-                if (!strcmp(int_mods.modules[j].name, module->imp[i].module->name)
+                if (module->imp[i].module && !strcmp(int_mods.modules[j].name, module->imp[i].module->name)
                         && module->imp[i].module->rev
                         && !strcmp(int_mods.modules[j].revision, module->imp[i].module->rev[0].date)) {
                     break;
