b26572e9566f61fef1dce6071d539211ac6f5737
Michal Vasko <mvasko@cesnet.cz>
2015-09-09 15:11:29 +0200

    xml parser DOC TODO updated with possible solution
    Will be tested later.

--- src/parser_xml.c
+++ src/parser_xml.c
@@ -90,7 +90,7 @@ transform_data_xml2json(struct ly_ctx *ctx, struct lyxml_elem *xml, int log)
         ns = lyxml_get_ns(xml, prefix);
         free(prefix);
         if (!ns) {
-            /* TODO a valid case if replacing an XPath in an augment part from a different model */
+            /* TODO a valid case if replacing an XPath in an augment part from a different model (won't happen if namespaces used in the augment get copied over as well) */
             if (log) {
                 LOGVAL(LYE_SPEC, LOGLINE(xml), "XML namespace with prefix \"%.*s\" not defined.", id_len, id);
             }
