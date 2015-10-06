faa14ed61d789acbc7348d1ac24924d9ac211e35
Radek Krejci <rkrejci@cesnet.cz>
2015-10-06 12:35:42 +0200

    tests MAINTENANCE code formatting
    follow the coding style - spaces instead of tabs and braces even
    in case of one-line if body.

--- tests/test_data_initialization.c
+++ tests/test_data_initialization.c
@@ -35,153 +35,152 @@
 #include "config.h"
 #include "../src/libyang.h"
 
-struct ly_ctx *ctx;
-struct lyd_node *root;
-struct lys_module *model;
+struct ly_ctx *ctx = NULL;
+struct lyd_node *root = NULL;
 
 int generic_init(char *config_file, char *yang_file, char *yang_folder)
 {
-	LYS_INFORMAT yang_format;
-	LYD_FORMAT in_format;
-	char *schema = NULL;
-	char *config = NULL;
-	struct stat sb;
-	int fd = 0;
+    LYS_INFORMAT yang_format;
+    LYD_FORMAT in_format;
+    char *schema = NULL;
+    char *config = NULL;
+    struct stat sb;
+    int fd = 0;
 
-	if (!config_file || !yang_file || !yang_folder)
-		goto error;
+    if (!config_file || !yang_file || !yang_folder) {
+        goto error;
+    }
 
-	yang_format = LYS_IN_YIN;
-	in_format = LYD_XML;
+    yang_format = LYS_IN_YIN;
+    in_format = LYD_XML;
 
-	ctx = ly_ctx_new(yang_folder);
-	if (!ctx)
-		goto error;
+    ctx = ly_ctx_new(yang_folder);
+    if (!ctx) {
+        goto error;
+    }
 
-	fd = open(yang_file, O_RDONLY);
-	if (fd == -1)
-		goto error;
-	if (fstat(fd, &sb) == -1)
-		goto error;
-	if (!S_ISREG(sb.st_mode))
-		goto error;
+    fd = open(yang_file, O_RDONLY);
+    if (fd == -1 || fstat(fd, &sb) == -1 || !S_ISREG(sb.st_mode)) {
+        goto error;
+    }
 
-	schema = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	close(fd);
+    schema = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
 
-	fd = open(config_file, O_RDONLY);
-	if (fd == -1)
-		goto error;
-	if (fstat(fd, &sb) == -1)
-		goto error;
-	if (!S_ISREG(sb.st_mode))
-		goto error;
+    fd = open(config_file, O_RDONLY);
+    if (fd == -1 || fstat(fd, &sb) == -1 || !S_ISREG(sb.st_mode)) {
+        goto error;
+    }
 
-	config = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	close(fd);
+    config = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
 
-	model = lys_parse(ctx, schema, yang_format);
-	if (!model)
-		goto error;
+    if (!lys_parse(ctx, schema, yang_format)) {
+        goto error;
+    }
 
-	root = lyd_parse(ctx, config, in_format, LYD_OPT_STRICT);
-	if (!root)
-		goto error;
+    root = lyd_parse(ctx, config, in_format, LYD_OPT_STRICT);
+    if (!root) {
+        goto error;
+    }
 
-	return 0;
+    return 0;
 
 error:
-	if (!schema)
-		munmap(schema, sb.st_size);
-	if (!config)
-		munmap(config, sb.st_size);
-	if (fd != -1)
-		close(fd);
+    if (!schema) {
+        munmap(schema, sb.st_size);
+    }
+    if (!config) {
+        munmap(config, sb.st_size);
+    }
+    if (fd != -1) {
+        close(fd);
+    }
 
-	return -1;
+    return -1;
 }
 
 static int setup_f()
 {
-	char *config_file = TESTS_DIR"/config/hello.xml";
-	char *yang_file = TESTS_DIR"/config/hello@2015-06-08.yin";
-	char *yang_folder = TESTS_DIR"/config";
-	int rc;
+    char *config_file = TESTS_DIR"/config/hello.xml";
+    char *yang_file = TESTS_DIR"/config/hello@2015-06-08.yin";
+    char *yang_folder = TESTS_DIR"/config";
+    int rc;
 
-	rc = generic_init(config_file, yang_file, yang_folder);
-	if (rc)
-		return -1;
+    rc = generic_init(config_file, yang_file, yang_folder);
+    if (rc) {
+        return -1;
+    }
 
-	return 0;
+    return 0;
 }
 
 static int teardown_f()
 {
-	lyd_free(root);
-	ly_ctx_destroy(ctx);
+    lyd_free(root);
+    ly_ctx_destroy(ctx);
 
-	return 0;
+    return 0;
 }
 
 static void test_ctx_new_destroy()
 {
-	ctx = ly_ctx_new(NULL);
-	if (!ctx)
-		fail();
+    ctx = ly_ctx_new(NULL);
+    if (!ctx) {
+        fail();
+    }
 
-	ly_ctx_destroy(ctx);
+    ly_ctx_destroy(ctx);
 }
-
 
 static void test_container_name()
 {
-	struct lyd_node *node;
-	const char *result = "";
+    struct lyd_node *node;
+    const char *result = "";
 
-	node = root;
-	result = node->schema->name;
+    node = root;
+    result = node->schema->name;
 
-	assert_string_equal("hello", result);
+    assert_string_equal("hello", result);
 }
 
 static void test_leaf_name()
 {
-	struct lyd_node *node;
-	const char *result;
+    struct lyd_node *node;
+    const char *result;
 
-	node = root;
-	result = node->schema->child->name;
+    node = root;
+    result = node->schema->child->name;
 
-	assert_string_equal("foo", result);
+    assert_string_equal("foo", result);
 }
 
 static void test_leaf_list_parameters()
 {
-	struct lyd_node *node;
-	struct lyd_node *tmp;
-	const char *name_result;
-	const char *str_result;
-	int int_result;
+    struct lyd_node *node;
+    struct lyd_node *tmp;
+    const char *name_result;
+    const char *str_result;
+    int int_result;
 
-	node = root;
-	tmp = node->child->next;
-	name_result = tmp->schema->name;
-	int_result = ((struct lyd_node_leaf_list *) tmp)->value.int32;
-	str_result = ((struct lyd_node_leaf_list *) tmp)->value_str;
+    node = root;
+    tmp = node->child->next;
+    name_result = tmp->schema->name;
+    int_result = ((struct lyd_node_leaf_list *)tmp)->value.int32;
+    str_result = ((struct lyd_node_leaf_list *)tmp)->value_str;
 
-	assert_int_equal(1234, int_result);
-	assert_string_equal("1234", str_result);
-	assert_string_equal("bar", name_result);
+    assert_int_equal(1234, int_result);
+    assert_string_equal("1234", str_result);
+    assert_string_equal("bar", name_result);
 }
 
 int main(void)
 {
-	const struct CMUnitTest tests[] = {
-		cmocka_unit_test(test_ctx_new_destroy),
-		cmocka_unit_test_setup_teardown(test_container_name, setup_f, teardown_f),
-		cmocka_unit_test_setup_teardown(test_leaf_name, setup_f, teardown_f),
-		cmocka_unit_test_setup_teardown(test_leaf_list_parameters, setup_f, teardown_f),
-	};
+    const struct CMUnitTest tests[] = {
+                    cmocka_unit_test(test_ctx_new_destroy),
+                    cmocka_unit_test_setup_teardown(test_container_name, setup_f, teardown_f),
+                    cmocka_unit_test_setup_teardown(test_leaf_name, setup_f, teardown_f),
+                    cmocka_unit_test_setup_teardown(test_leaf_list_parameters, setup_f, teardown_f), };
 
-	return cmocka_run_group_tests(tests, NULL, NULL);
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
