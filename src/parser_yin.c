d4e78dc1fe6a7c44761c8d0660465dbbb09f01d7
Radek Krejci <rkrejci@cesnet.cz>
2015-12-05 13:37:39 +0100

    yin parser BUGFIX enum and bits name is not an identifier
    RFC 6020 do not specifi enum's and bit's name as an identifier,
    so it can start e.g. by a digit. We did a check for identifier.

--- src/parser_yin.c
+++ src/parser_yin.c
@@ -556,9 +556,6 @@ fill_yin_type(struct lys_module *module, struct lys_node *parent, struct lyxml_e
             i++;
 
             GETVAL(value, next, "name");
-            if (check_identifier(value, LY_IDENT_SIMPLE, LOGLINE(next), NULL, NULL)) {
-                goto error;
-            }
             type->info.bits.bit[i].name = lydict_insert(module->ctx, value, strlen(value));
             if (read_yin_common(module, NULL, (struct lys_node *)&type->info.bits.bit[i], next, 0)) {
                 type->info.bits.count = i + 1;
@@ -730,9 +727,6 @@ fill_yin_type(struct lys_module *module, struct lys_node *parent, struct lyxml_e
             i++;
 
             GETVAL(value, next, "name");
-            if (check_identifier(value, LY_IDENT_SIMPLE, LOGLINE(next), NULL, NULL)) {
-                goto error;
-            }
             type->info.enums.enm[i].name = lydict_insert(module->ctx, value, strlen(value));
             if (read_yin_common(module, NULL, (struct lys_node *)&type->info.enums.enm[i], next, 0)) {
                 type->info.enums.count = i + 1;
