a53bf43f2146a1c25a1ece0f30e997f590d88a81
Radek Krejci <rkrejci@cesnet.cz>
2016-01-28 11:23:59 +0100

    ABI check FEATURE allow comparing 2 specific versions

--- doc/ABI/abi.sh
+++ doc/ABI/abi.sh
@@ -5,7 +5,16 @@ ABI_TRK=abi-tracker
 which $ABI_MON > /dev/null || exit 1
 which $ABI_TRK > /dev/null || exit 1
 
-if test $1; then
+if test -n "$2"; then
+	VER1="$1"
+	VER2="$2"
+elif test -n "$1"; then
+	if test "$1" = "help" -o "$1" = "-h" -o "$1" = "--help"; then
+		echo Usage:
+		echo \t$0 [number of last versions]
+		echo \t$0 [version1] [version2]
+		exit 0
+	fi
 	LIMIT="-limit $1"
 	l=$1
 fi
@@ -17,20 +26,30 @@ rm -rf src/libyang/*
 
 cd gitrepo
 git checkout devel
-for i in `git log --grep=VERSION --oneline | sed 's/ .* /_/'`; do
-	if test "$l" -eq 0; then
-		rm -rf ../abi_dump/libyang/${i//*_}
-		rm -rf ../installed/libyang/${i//*_}
+VERSIONS=`git log --grep=VERSION --oneline | sed 's/ .* /_/'`
+cd -
+for i in $VERSIONS; do
+	if test "$l" = "0"; then
+		rm -rf abi_dump/libyang/${i//*_}
+		rm -rf installed/libyang/${i//*_}
 	else
-		l=$(($l-1))
-		if test ! -f ../packages/libyang-${i//*_}.tgz; then
-			git checkout ${i//_*}
-			tar -czf ../packages/libyang-${i//*_}.tgz gitrepo
+		test -n "$l" && l=$(($l-1))
+		if test -n "$VER1"; then
+			if test "$VER1" != "${i//*_}" -a "$VER2" != "${i//*_}"; then
+				rm -rf abi_dump/libyang/${i//*_}
+				rm -rf installed/libyang/${i//*_}
+				continue
+			fi
 		fi
-		cp ../packages/libyang-${i//*_}.tgz ../src/libyang/
+		if test ! -f packages/libyang-${i//*_}.tgz; then
+			cd gitrepo
+			git checkout ${i//_*}
+			cd -
+			tar -czf packages/libyang-${i//*_}.tgz gitrepo
+		fi
+		cp packages/libyang-${i//*_}.tgz src/libyang/
 	fi
 done
-cd ..
 
 cp libyang.json libyang.aux
 abi-monitor -get -build $LIMIT libyang.aux
