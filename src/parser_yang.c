2a064658aa5c35010ce56acb1946f65516ba6b25
Pavol Vican <xvican01@stud.fit.vutbr.cz>
2016-02-02 22:54:34 +0100

    fix coding style

--- src/parser_yang.c
+++ src/parser_yang.c
@@ -23,39 +23,46 @@
 #include "parser_yang_bis.h"
 
 
-static int yang_check_string(struct lys_module *module, const char **target, char *what, char *where, char *value, int line){
-
+static int 
+yang_check_string(struct lys_module *module, const char **target, char *what, char *where, char *value, int line)
+{
     if (*target) {
-        LOGVAL(LYE_TOOMANY,line,what,where);
+        LOGVAL(LYE_TOOMANY, line, what, where);
         free(value);
         return 1;
     } else {
-        *target = lydict_insert_zc(module->ctx,value);
+        *target = lydict_insert_zc(module->ctx, value);
         return 0;
     }
 }
 
-int yang_read_common(struct lys_module *module,char *value, int type, int line) {
-	int ret=0;
+int 
+yang_read_common(struct lys_module *module, char *value, int type, int line) 
+{
+    int ret=0;
 
     switch (type) {
-        case MODULE_KEYWORD:
-            module->name=lydict_insert_zc(module->ctx,value);
-            break;
-        case NAMESPACE_KEYWORD:
-            ret = yang_check_string(module,&module->ns,"namespace","module",value,line);
-            break;
-    }   
+    case MODULE_KEYWORD:
+        module->name = lydict_insert_zc(module->ctx, value);
+        break;
+    case NAMESPACE_KEYWORD:
+        ret = yang_check_string(module, &module->ns, "namespace", "module", value, line);
+        break;
+    }
+
     return ret;
 }
 
-int yang_read_prefix(struct lys_module *module, void *save, char *value,int type,int line) {
+int 
+yang_read_prefix(struct lys_module *module, void *save, char *value, int type, int line) 
+{
     int ret;
 
     switch (type){
-    	case MODULE_KEYWORD:
-    		ret = yang_check_string(module,&module->prefix,"prefix","module",value,line);
-    		break;
-	}
+    case MODULE_KEYWORD:
+        ret = yang_check_string(module, &module->prefix, "prefix", "module", value, line);
+        break;
+    }
+    
     return ret;
 }
