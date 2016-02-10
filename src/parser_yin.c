218436dfb5e02f45a5c2afa69e3dde06b4617dd4
Radek Krejci <rkrejci@cesnet.cz>
2016-02-10 12:54:06 +0100

    yin parser BUGFIX accept empty strings in text/value elements of description, reference, etc.
    Fixes #20

--- src/parser_yin.c
+++ src/parser_yin.c
@@ -85,14 +85,15 @@ read_yin_subnode(struct ly_ctx *ctx, struct lyxml_elem *node, const char *name)
 
     /* there should be <text> child */
     if (!node->child || !node->child->name || strcmp(node->child->name, name)) {
-        LOGWRN("Expected \"%s\" element in \"%s\" element.", name, node->name);
+        LOGERR(LY_EVALID, "Expected \"%s\" element in \"%s\" element.", name, node->name);
+        LOGVAL(LYE_INARG, LOGLINE(node), name, node->name);
+        return NULL;
     } else if (node->child->content) {
         len = strlen(node->child->content);
         return lydict_insert(ctx, node->child->content, len);
+    } else {
+        return lydict_insert(ctx, "", 0);
     }
-
-    LOGVAL(LYE_INARG, LOGLINE(node), name, node->name);
-    return NULL;
 }
 
 /* logs directly */
