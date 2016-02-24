54f6fb32ae39279532fe1a9ace9055bbfe78dcc5
Radek Krejci <rkrejci@cesnet.cz>
2016-02-24 12:56:39 +0100

    MAINTENANCE correct license text in source codes
    THIS DOES NOT CHANGE THE LICENSE!
    
    We had an incomplete text of BSD 3-Clause license in source codes.
    So now the License text is stored in LICENSE file and the text in
    source codes just refers to the full text of BSD 3-Clause License.

--- src/printer_xml.c
+++ src/printer_xml.c
@@ -1,4 +1,3 @@
-
 /**
  * @file printer_xml.c
  * @author Michal Vasko <mvasko@cesnet.cz>
@@ -6,19 +5,13 @@
  *
  * Copyright (c) 2015 CESNET, z.s.p.o.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name of the Company nor the names of its contributors
- *    may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
+ * This source code is licensed under BSD 3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://opensource.org/licenses/BSD-3-Clause
  */
+
 #define _GNU_SOURCE
 
 #include <stdlib.h>
