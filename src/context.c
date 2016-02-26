8de098c4062eaeaea41e2b6a5aada725db1c0a08
Michal Vasko <mvasko@cesnet.cz>
2016-02-26 10:00:25 +0100

    all DOC white space

--- src/context.c
+++ src/context.c
@@ -8,7 +8,7 @@
  * This source code is licensed under BSD 3-Clause License (the "License").
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     https://opensource.org/licenses/BSD-3-Clause
  */
 
