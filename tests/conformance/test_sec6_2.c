225d2629dae5de0f59582850f8e20d5cc945f83d
Michal Vasko <mvasko@cesnet.cz>
2016-06-23 15:28:23 +0200

    tests CHANGE always test with autogenerated YIN modules as well

--- tests/conformance/test_sec6_2.c
+++ tests/conformance/test_sec6_2.c
@@ -15,6 +15,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <setjmp.h>
+#include <errno.h>
+#include <unistd.h>
 #include <cmocka.h>
 #include <string.h>
 
@@ -24,7 +26,6 @@
 #define TEST_DIR "sec6_2"
 #define TEST_NAME test_sec6_2
 #define TEST_SCHEMA_COUNT 4
-#define TEST_SCHEMA_FORMAT "yang"
 #define TEST_SCHEMA_LOAD_FAIL 0,1,1,1
 #define TEST_DATA_FILE_COUNT 0
 #define TEST_DATA_FILE_LOAD_FAIL 0
@@ -74,27 +75,57 @@ TEST_NAME(void **state)
     struct state *st = (*state);
     const int schemas_fail[] = {TEST_SCHEMA_LOAD_FAIL};
     const int data_files_fail[] = {TEST_DATA_FILE_LOAD_FAIL};
-    char buf[512];
+    char buf[1024];
+    LYS_INFORMAT schema_format = LYS_IN_YANG;
     const struct lys_module *mod;
-    int i;
+    int i, j, ret;
 
-    for (i = 0; i < TEST_SCHEMA_COUNT; ++i) {
-        sprintf(buf, TESTS_DIR "/conformance/" TEST_DIR "/mod%d.%s", i + 1, TEST_SCHEMA_FORMAT);
-        mod = lys_parse_path(st->ctx, buf, (!strcmp(TEST_SCHEMA_FORMAT, "yang") ? LYS_IN_YANG : LYS_IN_YIN));
-        if (schemas_fail[i]) {
-            assert_ptr_equal(mod, NULL);
-        } else {
-            assert_ptr_not_equal(mod, NULL);
+    for (i = 0; i < 2; ++i) {
+        for (j = 0; j < TEST_SCHEMA_COUNT; ++j) {
+            sprintf(buf, TESTS_DIR "/conformance/" TEST_DIR "/mod%d.%s", j + 1, (schema_format == LYS_IN_YANG ? "yang" : "yin"));
+            mod = lys_parse_path(st->ctx, buf, schema_format);
+            if (schemas_fail[j]) {
+                assert_ptr_equal(mod, NULL);
+            } else {
+                assert_ptr_not_equal(mod, NULL);
+            }
         }
-    }
 
-    for (i = 0; i < TEST_DATA_FILE_COUNT; ++i) {
-        sprintf(buf, TESTS_DIR "/conformance/data%d.xml", i + 1);
-        st->node = lyd_parse_path(st->ctx, buf, LYD_XML, LYD_OPT_CONFIG);
-        if (data_files_fail[i]) {
-            assert_ptr_not_equal(st->node, NULL);
+        for (j = 0; j < TEST_DATA_FILE_COUNT; ++j) {
+            sprintf(buf, TESTS_DIR "/conformance/data%d.xml", j + 1);
+            st->node = lyd_parse_path(st->ctx, buf, LYD_XML, LYD_OPT_CONFIG);
+            if (data_files_fail[j]) {
+                assert_ptr_not_equal(st->node, NULL);
+            } else {
+                assert_ptr_equal(st->node, NULL);
+            }
+        }
+
+        if (schema_format == LYS_IN_YANG) {
+            /* convert the modules */
+            for (j = 0; j < TEST_SCHEMA_COUNT; ++j) {
+                sprintf(buf, TESTS_DIR "/../yang2yin "
+                             TESTS_DIR "/conformance/" TEST_DIR "/mod%d.yang "
+                             TESTS_DIR "/conformance/" TEST_DIR "/mod%d.yin", j + 1, j + 1);
+                ret = system(buf);
+                if (ret == -1) {
+                    fprintf(stderr, "system() failed (%s).\n", strerror(errno));
+                    fail();
+                } else if (WEXITSTATUS(ret) != 0) {
+                    fprintf(stderr, "Executing command \"%s\" finished with %d.\n", buf, WEXITSTATUS(ret));
+                    fail();
+                }
+            }
+
+            schema_format = LYS_IN_YIN;
         } else {
-            assert_ptr_equal(st->node, NULL);
+            /* remove the modules */
+            for (j = 0; j < TEST_SCHEMA_COUNT; ++j) {
+                sprintf(buf, TESTS_DIR "/conformance/" TEST_DIR "/mod%d.yin", j + 1);
+                if (unlink(buf)) {
+                    fprintf(stderr, "unlink() on \"%s\" failed (%s).\n", buf, strerror(errno));
+                }
+            }
         }
     }
 }
