fe98975b7afd737b6fbf86c2797c6a902b015b4b
Michal Vasko <mvasko@cesnet.cz>
2016-09-08 08:47:26 +0200

    resolve BUGFIX properly find when data context node

--- src/resolve.c
+++ src/resolve.c
@@ -5033,26 +5033,34 @@ resolve_when_ctx_node(struct lyd_node *node, struct lys_node *schema, struct lyd
 
     resolve_when_ctx_snode(schema, &schema, &node_type);
 
-    /* get node depths */
-    for (parent = node, data_depth = 0; parent; parent = parent->parent, ++data_depth);
-    for (sparent = schema, schema_depth = 0;
-            sparent;
-            sparent = (sparent->nodetype == LYS_AUGMENT ? ((struct lys_node_augment *)sparent)->target : sparent->parent)) {
-        if (sparent->nodetype & (LYS_CONTAINER | LYS_LEAF | LYS_LEAFLIST | LYS_LIST | LYS_ANYDATA | LYS_NOTIF | LYS_RPC)) {
-            ++schema_depth;
+    if (node_type == LYXP_NODE_ELEM) {
+        /* standard element context node */
+        for (parent = node, data_depth = 0; parent; parent = parent->parent, ++data_depth);
+        for (sparent = schema, schema_depth = 0;
+                sparent;
+                sparent = (sparent->nodetype == LYS_AUGMENT ? ((struct lys_node_augment *)sparent)->target : sparent->parent)) {
+            if (sparent->nodetype & (LYS_CONTAINER | LYS_LEAF | LYS_LEAFLIST | LYS_LIST | LYS_ANYDATA | LYS_NOTIF | LYS_RPC)) {
+                ++schema_depth;
+            }
         }
-    }
-    if (data_depth < schema_depth) {
-        return -1;
-    }
+        if (data_depth < schema_depth) {
+            return -1;
+        }
 
-    if (schema_depth) {
         /* find the corresponding data node */
         for (i = 0; i < data_depth - schema_depth; ++i) {
             node = node->parent;
         }
         if (node->schema != schema) {
             return -1;
+        }
+    } else {
+        /* root context node */
+        while (node->parent) {
+            node = node->parent;
+        }
+        while (node->prev->next) {
+            node = node->prev;
         }
     }
 
