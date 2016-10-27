fb7156e91ab0a50258f20c0c8f81cb1d1d954836
Radek Krejci <rkrejci@cesnet.cz>
2016-10-27 13:39:56 +0200

    data parser CHANGE keep all empty containers being parsed despite the data tree type
    In LYD_OPT_DATA and LYD_OPT_CONFIG trees we add empty containers as default
    nodes to be part of accessible tree. In other data types we do not do that,
    but we also removes empty non-presence containers when such a data tree is
    parsed. A more proper and clean way is to keep such elements, but mark them
    also as a default node (so they are not printed by default). It also save some
    resources avoiding removing empty container and them recreating it as a
    default node.
    
    Fixes #174

--- src/resolve.c
+++ src/resolve.c
@@ -6789,7 +6789,6 @@ resolve_unres_data_item(struct lyd_node *node, enum UNRES_ITEM type)
     int rc;
     struct lyd_node_leaf_list *leaf;
     struct lys_node_leaf *sleaf;
-    struct lyd_node *parent;
 
     leaf = (struct lyd_node_leaf_list *)node;
     sleaf = (struct lys_node_leaf *)leaf->schema;
@@ -6837,15 +6836,6 @@ resolve_unres_data_item(struct lyd_node *node, enum UNRES_ITEM type)
         }
         break;
 
-    case UNRES_EMPTYCONT:
-        do {
-            parent = node->parent;
-            lyd_free(node);
-            node = parent;
-        } while (node && (node->schema->nodetype == LYS_CONTAINER) && !node->child
-                 && !((struct lys_node_container *)node->schema)->presence);
-        break;
-
     default:
         LOGINT;
         return -1;
@@ -6867,7 +6857,7 @@ unres_data_add(struct unres_data *unres, struct lyd_node *node, enum UNRES_ITEM 
 {
     assert(unres && node);
     assert((type == UNRES_LEAFREF) || (type == UNRES_INSTID) || (type == UNRES_WHEN) || (type == UNRES_MUST)
-           || (type == UNRES_MUST_INOUT) || (type == UNRES_UNION) || (type == UNRES_EMPTYCONT));
+           || (type == UNRES_MUST_INOUT) || (type == UNRES_UNION));
 
     unres->count++;
     unres->node = ly_realloc(unres->node, unres->count * sizeof *unres->node);
