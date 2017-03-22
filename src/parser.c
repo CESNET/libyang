b08fc40c989a0d87d71dfe3afc2fdfed5211a957
Radek Krejci <rkrejci@cesnet.cz>
2017-03-22 10:54:01 +0100

    parsers BUGFIX making canonical form of decimal64 value
    in case of havin zeros after decimal point (e.g. 0.01 for fraction-digits 2),
    the value was incorrectly printed with not enough leading zeros.
    
    Fixes #289

--- src/parser.c
+++ src/parser.c
@@ -1146,8 +1146,10 @@ make_canonical(struct ly_ctx *ctx, int type, const char **value, void *data1, vo
         if (num) {
             count = sprintf(buf, "%"PRId64" ", num);
             if ((count - 1) <= c) {
-                /* we have 0. value, add a space for the leading zero */
-                count = sprintf(buf, "0%"PRId64" ", num);
+                /* we have 0. value, print the value with the leading zeros
+                 * (one for 0. and also keep the correct with of num according
+                 * to fraction-digits value) */
+                count = sprintf(buf, "0%0*"PRId64" ", c, num);
             }
             for (i = c, j = 1; i > 0 ; i--) {
                 if (j && i > 1 && buf[count - 2] == '0') {
