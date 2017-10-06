6a1efdf60203cdfdb3fc718226f7b044c728525e
Michal Vasko <mvasko@cesnet.cz>
2017-10-06 14:38:08 +0200

    resolve CHANGE use data callback for unresolved data identityref values

--- src/resolve.c
+++ src/resolve.c
@@ -5303,9 +5303,10 @@ struct lys_ident *
 resolve_identref(struct lys_type *type, const char *ident_name, struct lyd_node *node, struct lys_module *mod, int dflt)
 {
     const char *mod_name, *name;
+    char *str;
     int mod_name_len, nam_len, rc;
     int need_implemented = 0;
-    unsigned int u, i, j;
+    unsigned int i, j;
     struct lys_ident *der, *cur;
     struct lys_module *imod = NULL, *m;
 
@@ -5338,12 +5339,24 @@ resolve_identref(struct lys_type *type, const char *ident_name, struct lyd_node 
             }
         }
     } else {
-        /* solving identityref in data - get the (implemented) module from the context */
-        u = 0;
-        while ((imod = (struct lys_module *)ly_ctx_get_module_iter(mod->ctx, &u))) {
+        /* solving identityref in data - get the module from the context */
+        for (i = 0; i < (unsigned)mod->ctx->models.used; ++i) {
+            imod = mod->ctx->models.list[i];
             if (!strncmp(mod_name, imod->name, mod_name_len) && !imod->name[mod_name_len]) {
                 break;
             }
+            imod = NULL;
+        }
+    }
+
+    if (!dflt && (!imod || !imod->implemented) && mod->ctx->data_clb) {
+        /* the needed module was not found, but it may have been expected so call the data callback */
+        if (imod) {
+            mod->ctx->data_clb(mod->ctx, imod->name, imod->ns, LY_MODCLB_NOT_IMPLEMENTED, mod->ctx->data_clb_data);
+        } else {
+            str = strndup(mod_name, mod_name_len);
+            imod = (struct lys_module *)mod->ctx->data_clb(mod->ctx, str, NULL, 0, mod->ctx->data_clb_data);
+            free(str);
         }
     }
     if (!imod) {
@@ -5351,7 +5364,7 @@ resolve_identref(struct lys_type *type, const char *ident_name, struct lyd_node 
     }
 
     if (m != imod || lys_main_module(type->parent->module) != mod) {
-        /* we are solving default statement in schema AND the type is not referencing the same schema,
+        /* the type is not referencing the same schema,
          * THEN, we may need to make the module with the identity implemented, but only if it really
          * contains the identity */
         if (!imod->implemented) {
@@ -5402,8 +5415,8 @@ resolve_identref(struct lys_type *type, const char *ident_name, struct lyd_node 
 
             if (cur->der) {
                 /* there are some derived identities */
-                for (u = 0; u < cur->der->number; u++) {
-                    der = (struct lys_ident *)cur->der->set.g[u]; /* shortcut */
+                for (j = 0; j < cur->der->number; j++) {
+                    der = (struct lys_ident *)cur->der->set.g[j]; /* shortcut */
                     if (!strcmp(der->name, name) && lys_main_module(der->module) == imod) {
                         /* we have match */
                         cur = der;
