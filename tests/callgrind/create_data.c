22fdd754a8ce03150829cc16d7175e5a0ae6bb63
Michal Vasko <mvasko@cesnet.cz>
2017-10-24 09:02:52 +0200

    tests NEW callgrind target running benchmarks with callgrind

---
+++ tests/callgrind/create_data.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+
+#include <libyang/libyang.h>
+
+#define SCHEMA "files/ietf-interfaces.yang"
+#define SCHEMA2 "files/ietf-ip.yang"
+#define SCHEMA3 "files/iana-if-type.yang"
+
+int
+main(void)
+{
+    int ret = 0;
+    struct ly_ctx *ctx = NULL;
+    struct lyd_node *data = NULL, *node;
+
+    ctx = ly_ctx_new(NULL, 0);
+    if (!ctx) {
+        ret = 1;
+        goto finish;
+    }
+
+    if (!lys_parse_path(ctx, SCHEMA, LYS_YANG)) {
+        ret = 1;
+        goto finish;
+    }
+
+    if (!lys_parse_path(ctx, SCHEMA2, LYS_YANG)) {
+        ret = 1;
+        goto finish;
+    }
+
+    if (!lys_parse_path(ctx, SCHEMA3, LYS_YANG)) {
+        ret = 1;
+        goto finish;
+    }
+
+    data = lyd_new_path(NULL, ctx, "/ietf-interfaces:interfaces/interface[name='eth0']/ietf-ip:ipv4/ietf-ip:address[ietf-ip:ip='47.250.10.1']/ietf-ip:prefix-length", "16", LYD_ANYDATA_CONSTSTRING, 0);
+    if (!data) {
+        ret = 1;
+        goto finish;
+    }
+
+    node = lyd_new_path(data, ctx, "/ietf-interfaces:interfaces/interface[name='eth0']/type", "iana-if-type:ethernetCsmacd", LYD_ANYDATA_CONSTSTRING, 0);
+    if (!node) {
+        ret = 1;
+        goto finish;
+    }
+
+    node = lyd_new_path(data, ctx, "/ietf-interfaces:interfaces/interface[name='eth0']/ietf-ip:ipv6/ietf-ip:address[ietf-ip:ip='fec0::1']/ietf-ip:prefix-length", "48", LYD_ANYDATA_CONSTSTRING, 0);
+    if (!node) {
+        ret = 1;
+        goto finish;
+    }
+
+    node = lyd_new_path(data, ctx, "/ietf-interfaces:interfaces/interface[name='eth0']/ietf-ip:ipv6/ietf-ip:mtu", "1500", LYD_ANYDATA_CONSTSTRING, 0);
+    if (!node) {
+        ret = 1;
+        goto finish;
+    }
+
+    if (lyd_validate(&data, LYD_OPT_DATA | LYD_OPT_DATA_NO_YANGLIB, NULL)) {
+        ret = 1;
+        goto finish;
+    }
+
+finish:
+    lyd_free_withsiblings(data);
+    ly_ctx_destroy(ctx, NULL);
+    return ret;
+}
---
+++ tests/callgrind/validate.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+#include <libyang/libyang.h>
+
+int
+main(int argc, char **argv)
+{
+    int i;
+    struct ly_ctx *ctx;
+    struct lyd_node *data;
+
+    if (argc < 3) {
+        return 1;
+    }
+
+    ctx = ly_ctx_new(NULL, 0);
+    if (!ctx) {
+        return 1;
+    }
+
+    for (i = 1; i < argc - 1; ++i) {
+        if (!lys_parse_path(ctx, argv[i], LYS_YANG)) {
+            ly_ctx_destroy(ctx, NULL);
+            return 1;
+        }
+    }
+
+    data = lyd_parse_path(ctx, argv[argc - 1], LYD_XML, LYD_OPT_STRICT | LYD_OPT_DATA_NO_YANGLIB);
+    if (!data) {
+        ly_ctx_destroy(ctx, NULL);
+        return 1;
+    }
+
+    lyd_free_withsiblings(data);
+    ly_ctx_destroy(ctx, NULL);
+    return 0;
+}
