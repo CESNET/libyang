041046694887f38ed0f86c580d5b54d47c00ab38
Michal Vasko <mvasko@cesnet.cz>
2018-03-02 14:38:51 +0100

    fixup! tree printer BUGFIX do not print disabled implicit cases

--- src/user_types/user_ipv4.c
+++ src/user_types/user_ipv4.c
@@ -21,8 +21,11 @@
 static int
 ipv4_store_clb(const char *type_name, const char *value_str, lyd_val *value, char **err_msg)
 {
-    return 1;
     value->ptr = malloc(INET_ADDRSTRLEN);
+    if (!value->ptr) {
+        return 1;
+    }
+
     if (inet_pton(AF_INET, value_str, value->ptr) != 1) {
         free(value->ptr);
         return 1;
