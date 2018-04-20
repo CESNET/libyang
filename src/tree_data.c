CESNET/libyang

commit 563cfeaa32a9f637672bfe29c177918ba56675cd

data tree CHANGE new function lyd_set_merge

src/tree_data.c
@@ -5673,6 +5673,50 @@ ly_set_add(struct ly_set *set, void *node, int options)
 }
 
 API int
+ly_set_merge(struct ly_set *trg, struct ly_set *src, int options)
+{
+    unsigned int i, ret;
+    void **new;
+
+    if (!trg) {
+        ly_errno = LY_EINVAL;
+        return -1;
+    }
+
+    if (!src) {
+        return 0;
+    }
+
+    if (!(options & LY_SET_OPT_USEASLIST)) {
+        /* remove duplicates */
+        i = 0;
+        while (i < src->number) {
+            if (ly_set_contains(trg, src->set.g[i]) > -1) {
+                ly_set_rm_index(src, i);
+            } else {
+                ++i;
+            }
+        }
+    }
+
+    /* allocate more memory if needed */
+    if (trg->size < trg->number + src->number) {
+        new = realloc(trg->set.g, (trg->number + src->number) * sizeof *(trg->set.g));
+        LY_CHECK_ERR_RETURN(!new, LOGMEM, -1);
+        trg->size = trg->number + src->number;
+        trg->set.g = new;
+    }
+
+    /* copy contents from src into trg */
+    memcpy(trg->set.g + trg->number, src->set.g, src->number * sizeof *(src->set.g));
+    ret = src->number;
+
+    /* cleanup */
+    ly_set_free(src);
+    return ret;
+}
+
+API int
 ly_set_rm_index(struct ly_set *set, unsigned int index)
 {
     if (!set || (index + 1) > set->number) {

src/libyang.h.in
@@ -1553,6 +1553,20 @@ struct ly_set *ly_set_dup(const struct ly_set *set);
 int ly_set_add(struct ly_set *set, void *node, int options);
 
 /**
+ * @brief Add all objects from \p src to \p trg.
+ *
+ * Since it is a set, the function checks for duplicities.
+ * After success, \p src is completely freed.
+ *
+ * @param[in] trg Target (result) set.
+ * @param[in] src Source set.
+ * @param[in] options Options to change behavior of the function. Accepted options are:
+ * - #LY_SET_OPT_USEASLIST - do not check for duplicities
+ * @return -1 on failure, number of objects added into \p trg on success.
+ */
+int ly_set_merge(struct ly_set *trg, struct ly_set *src, int options);
+
+/**
  * @brief Get know if the set contains the specified object.
  * @param[in] set Set to explore.
  * @param[in] node Object to be found in the set.

commit 0420c22cb3bdb5ec656a10acdfd8cf8d84c24316

data tree BUGFIX update set number in ly_set_merge
The target set should have its number updated after the merge
operation.

Fixes: 563cfeaa32a9 ("data tree CHANGE new function lyd_set_merge")

src/tree_data.c
@@ -5716,6 +5716,7 @@ ly_set_merge(struct ly_set *trg, struct ly_set *src, int options)
     /* copy contents from src into trg */
     memcpy(trg->set.g + trg->number, src->set.g, src->number * sizeof *(src->set.g));
     ret = src->number;
+    trg->number += ret;
 
     /* cleanup */
     ly_set_free(src);