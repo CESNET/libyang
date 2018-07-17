74e67f79d63d77049af1153178f5b9ae5d49492e
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2018-07-17 18:53:30 +0200

    tests ADD test case with many annoted child elements

--- tests/data/test_lyb.c
+++ tests/data/test_lyb.c
@@ -234,6 +234,27 @@ test_types(void **state)
 }
 
 static void
+test_many_child_annot(void **state)
+{
+    struct state *st = (*state);
+    int ret;
+
+    ly_ctx_set_searchdir(st->ctx, TESTS_DIR"/data/files");
+    assert_non_null(ly_ctx_load_module(st->ctx, "annotations", NULL));
+
+    st->dt1 = lyd_parse_path(st->ctx, TESTS_DIR"/data/files/many-childs-annot.xml", LYD_XML, LYD_OPT_CONFIG);
+    assert_ptr_not_equal(st->dt1, NULL);
+
+    ret = lyd_print_mem(&st->mem, st->dt1, LYD_LYB, LYP_WITHSIBLINGS);
+    assert_int_equal(ret, 0);
+
+    st->dt2 = lyd_parse_mem(st->ctx, st->mem, LYD_LYB, LYD_OPT_CONFIG);
+    assert_ptr_not_equal(st->dt2, NULL);
+
+    check_data_tree(st->dt1, st->dt2);
+}
+
+static void
 test_ietf_interfaces(void **state)
 {
     struct state *st = (*state);
@@ -302,6 +323,7 @@ main(void)
         cmocka_unit_test_setup_teardown(test_ietf_interfaces, setup_f, teardown_f),
         cmocka_unit_test_setup_teardown(test_origin, setup_f, teardown_f),
         cmocka_unit_test_setup_teardown(test_types, setup_f, teardown_f),
+        cmocka_unit_test_setup_teardown(test_many_child_annot, setup_f, teardown_f),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
