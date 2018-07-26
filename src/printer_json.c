CESNET/libyang

commit c27114c87f87475d20e945ee79058dac86fa9886

json printer BUGFIX format of the printed boolean, decimal64, int64 and uint64 values
All the mentioned types are supposed to be printed as JSON strings
but they were printed in a different way.

src/printer_json.c
@@ -115,19 +115,19 @@ json_print_leaf(struct lyout *out, int level, const struct lyd_node *node, int o
     case LY_TYPE_ENUM:
     case LY_TYPE_IDENT:
     case LY_TYPE_INST:
+    case LY_TYPE_DEC64:
+    case LY_TYPE_INT64:
+    case LY_TYPE_UINT64:
+    case LY_TYPE_BOOL:
         json_print_string(out, leaf->value_str);
         break;
 
-    case LY_TYPE_BOOL:
-    case LY_TYPE_DEC64:
     case LY_TYPE_INT8:
     case LY_TYPE_INT16:
     case LY_TYPE_INT32:
-    case LY_TYPE_INT64:
     case LY_TYPE_UINT8:
     case LY_TYPE_UINT16:
     case LY_TYPE_UINT32:
-    case LY_TYPE_UINT64:
         ly_print(out, "%s", leaf->value_str[0] ? leaf->value_str : "null");
         break;
 

tests/data/files/all-data.xml
@@ -7,7 +7,8 @@
   <leaf6>60000</leaf6>
   <leaf7>4000000000</leaf7>
   <leaf8>5000000000</leaf8>
-  <leaf9>25.012345678</leaf9>
+  <leaf9a>25.012345678</leaf9a>
+  <leaf10>false</leaf10>
   <leaf11>two</leaf11>
   <leaf12>flag0 flag2</leaf12>
   <leaf13>somebase64string</leaf13>

tests/data/files/all.yin
@@ -82,17 +82,20 @@
       <type name="uint64"/>
     </leaf>
     <choice name="choic1">
-      <default value="leaf10"/>
-      <leaf name="leaf9">
+      <default value="leaf9b"/>
+      <leaf name="leaf9a">
         <type name="decimal64">
           <fraction-digits value="9"/>
         </type>
       </leaf>
-      <leaf name="leaf10">
+      <leaf name="leaf9b">
         <type name="boolean"/>
         <default value="false"/>
       </leaf>
     </choice>
+    <leaf name="leaf10">
+      <type name="boolean"/>
+    </leaf>
     <leaf name="leaf11">
       <type name="enumeration">
         <enum name="one"/>

commit 0ddd003291fb9bc686c2306dfe16bdd6681cb900

json: print all numeric types as json numbers
There is no limit in the size of numbers in the JSON specification:

  http://json.org/

There is no reason to treat 64 bit numbers differently. Print all
YANG numeric types as JSON numbers.

Fixes: c27114c87f87 ("json printer BUGFIX format of the printed boolean, decimal64, int64 and uint64 values")

Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

src/printer_json.c
@@ -91,19 +91,19 @@ json_print_attrs(struct lyout *out, int level, const struct lyd_node *node, cons
         case LY_TYPE_BITS:
         case LY_TYPE_ENUM:
         case LY_TYPE_INST:
-        case LY_TYPE_INT64:
-        case LY_TYPE_UINT64:
-        case LY_TYPE_DEC64:
             json_print_string(out, attr->value_str);
             break;
 
         case LY_TYPE_INT8:
         case LY_TYPE_INT16:
         case LY_TYPE_INT32:
+        case LY_TYPE_INT64:
         case LY_TYPE_UINT8:
         case LY_TYPE_UINT16:
         case LY_TYPE_UINT32:
+        case LY_TYPE_UINT64:
         case LY_TYPE_BOOL:
+        case LY_TYPE_DEC64:
             ly_print(out, "%s", attr->value_str[0] ? attr->value_str : "null");
             break;
 
@@ -171,20 +171,20 @@ contentprint:
     case LY_TYPE_BITS:
     case LY_TYPE_ENUM:
     case LY_TYPE_INST:
-    case LY_TYPE_INT64:
-    case LY_TYPE_UINT64:
     case LY_TYPE_UNION:
-    case LY_TYPE_DEC64:
         json_print_string(out, leaf->value_str);
         break;
 
     case LY_TYPE_INT8:
     case LY_TYPE_INT16:
     case LY_TYPE_INT32:
+    case LY_TYPE_INT64:
     case LY_TYPE_UINT8:
     case LY_TYPE_UINT16:
     case LY_TYPE_UINT32:
+    case LY_TYPE_UINT64:
     case LY_TYPE_BOOL:
+    case LY_TYPE_DEC64:
         ly_print(out, "%s", leaf->value_str[0] ? leaf->value_str : "null");
         break;
 