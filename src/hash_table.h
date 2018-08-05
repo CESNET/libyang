9cf9cd368de2f252d771b025c79dc6db81f15f3d
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2018-08-05 23:04:36 +0200

    lydict CHANGE struct definition to use generic hash table

--- src/hash_table.h
+++ src/hash_table.h
@@ -22,50 +22,6 @@
 #include "dict.h"
 
 /**
- * size of the dictionary for each context
- */
-#define DICT_SIZE 1024
-
-/**
- * record of the dictionary
- * TODO: save the next pointer by different collision strategy, will need to
- * make dictionary size dynamic
- */
-struct dict_rec {
-    struct dict_rec *next;
-    char *value;
-    uint32_t refcount:22;
-    uint32_t len:10;
-#define DICT_REC_MAXCOUNT 0x003fffff
-#define DICT_REC_MAXLEN   0x000003ff
-};
-
-/**
- * dictionary to store repeating strings
- * TODO: make it variable size
- */
-struct dict_table {
-    struct dict_rec recs[DICT_SIZE];
-    int hash_mask;
-    uint32_t used;
-    pthread_mutex_t lock;
-};
-
-/**
- * @brief Initiate content (non-zero values) of the dictionary
- *
- * @param[in] dict Dictionary table to initiate
- */
-void lydict_init(struct dict_table *dict);
-
-/**
- * @brief Cleanup the dictionary content
- *
- * @param[in] dict Dictionary table to cleanup
- */
-void lydict_clean(struct dict_table *dict);
-
-/**
  * @brief Compute hash from (several) string(s).
  *
  * Usage:
@@ -127,6 +83,33 @@ struct hash_table {
     uint16_t rec_size;    /* real size (in bytes) of one record for accessing recs array */
     unsigned char *recs;  /* pointer to the hash table itself (array of struct ht_rec) */
 };
+
+struct dict_rec {
+    char *value;
+    uint32_t refcount;
+};
+
+/**
+ * dictionary to store repeating strings
+ */
+struct dict_table {
+    struct hash_table *hash_tab;
+    pthread_mutex_t lock;
+};
+
+/**
+ * @brief Initiate content (non-zero values) of the dictionary
+ *
+ * @param[in] dict Dictionary table to initiate
+ */
+void lydict_init(struct dict_table *dict);
+
+/**
+ * @brief Cleanup the dictionary content
+ *
+ * @param[in] dict Dictionary table to cleanup
+ */
+void lydict_clean(struct dict_table *dict);
 
 /**
  * @brief Create new hash table.
