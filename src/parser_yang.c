CESNET/libyang

commit 78ef03a8b897be919eb181e4e92789393ecf2a3e

yang parser MAINTENANCE minor refactor

src/parser_yang.c
@@ -2606,7 +2606,6 @@ yang_parse_ext_substatement(struct lys_module *module, struct unres_schema *unre
 struct lys_module *
 yang_read_module(struct ly_ctx *ctx, const char* data, unsigned int size, const char *revision, int implement)
 {
-
     struct lys_module *module = NULL, *tmp_mod;
     struct unres_schema *unres = NULL;
     struct lys_node *node = NULL;
@@ -2651,9 +2650,18 @@ yang_read_module(struct ly_ctx *ctx, const char* data, unsigned int size, const 
         if (unres->count && resolve_unres_schema(module, unres)) {
             goto error;
         }
+
+        /* check correctness of includes */
+        if (lyp_check_include_missing(module)) {
+            goto error;
+        }
     }
 
     lyp_sort_revisions(module);
+
+    if (lyp_rfn_apply_ext(module) || lyp_deviation_apply_ext(module)) {
+        goto error;
+    }
 
     if (revision) {
         /* check revision of the parsed model */
@@ -2666,15 +2674,6 @@ yang_read_module(struct ly_ctx *ctx, const char* data, unsigned int size, const 
 
     /* add into context if not already there */
     if (!ret) {
-        /* check correctness of includes */
-        if (lyp_check_include_missing(module)) {
-            goto error;
-        }
-
-        if (lyp_rfn_apply_ext(module) || lyp_deviation_apply_ext(module)) {
-            goto error;
-        }
-
         if (lyp_ctx_add_module(module)) {
             goto error;
         }
@@ -2692,16 +2691,16 @@ yang_read_module(struct ly_ctx *ctx, const char* data, unsigned int size, const 
         lys_free(tmp_mod, NULL, 0, 0);
     }
 
-    lyp_check_circmod_pop(ctx);
     unres_schema_free(NULL, &unres, 0);
+    lyp_check_circmod_pop(ctx);
     LOGVRB("Module \"%s%s%s\" successfully parsed as %s.", module->name, (module->rev_size ? "@" : ""),
            (module->rev_size ? module->rev[0].date : ""), (module->implemented ? "implemented" : "imported"));
     return module;
 
 error:
     /* cleanup */
-    lyp_check_circmod_pop(ctx);
     unres_schema_free(module, &unres, 1);
+
     if (!module) {
         if (ly_vecode(ctx) != LYVE_SUBMODULE) {
             LOGERR(ctx, ly_errno, "Module parsing failed.");
@@ -2715,6 +2714,7 @@ error:
         LOGERR(ctx, ly_errno, "Module parsing failed.");
     }
 
+    lyp_check_circmod_pop(ctx);
     lys_sub_module_remove_devs_augs(module);
     lyp_del_includedup(module, 1);
     lys_free(module, NULL, 0, 1);

commit ceaf8b475e2cad8d866c000aaa7b9aa03451bb21

Fix dangling parsing_sub_modules pointer
This change fixes a dangling pointer and memory leak introduced by the refactor
in 78ef03a.

In 78ef03a, the call to `lyp_check_circmod_pop()` (under the `error` label) was
moved to after the early `if (!module)` return. This meant that if `module` had
been freed above due to an error (see line 2637) and we entered the early
return, `lyp_check_circmod_pop()` would not be called.
`ctx->models->parsing_sub_modules` would now contain a danging pointer to a
module whose memory had been freed. Subsequent attempts to use the list
could result in undefined behavior, including a segmentation fault.

The fix is to pop the module off the list before we free it.

src/parser_yang.c
@@ -2630,6 +2630,10 @@ yang_read_module(struct ly_ctx *ctx, const char* data, unsigned int size, const 
     ret = yang_parse_mem(module, NULL, unres, data, size, &node);
     if (ret == -1) {
         if (ly_vecode(ctx) == LYVE_SUBMODULE) {
+            /* Remove this module from the list of processed modules,
+               as we're about to free it */
+            lyp_check_circmod_pop(module);
+
             free(module);
             module = NULL;
         } else {