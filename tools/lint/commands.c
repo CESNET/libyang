f8e9aa4ff3e3f75ff5f907ae5105f027b92ec226
Michal Vasko <mvasko@cesnet.cz>
2018-09-19 10:54:18 +0200

    yanglint CHANGE allow working with lyb data files

--- tools/lint/commands.c
+++ tools/lint/commands.c
@@ -73,7 +73,7 @@ cmd_print_help(void)
 void
 cmd_data_help(void)
 {
-    printf("data [-(-s)trict] [-t TYPE] [-d DEFAULTS] [-o <output-file>] [-f (xml | json)] [-r <running-file-name>]\n");
+    printf("data [-(-s)trict] [-t TYPE] [-d DEFAULTS] [-o <output-file>] [-f (xml | json | lyb)] [-r <running-file-name>]\n");
     printf("     <data-file-name> [<RPC/action-data-file-name> | <yang-data name>]\n\n");
     printf("Accepted TYPEs:\n");
     printf("\tauto       - resolve data type (one of the following) automatically (as pyang does),\n");
@@ -436,6 +436,8 @@ detect_data_format(char *filepath)
         return LYD_XML;
     } else if (len >= 6 && !strcmp(&filepath[len - 5], ".json")) {
         return LYD_JSON;
+    } else if (len >= 5 && !strcmp(&filepath[len - 4], ".lyb")) {
+        return LYD_LYB;
     } else {
         return LYD_UNKNOWN;
     }
@@ -454,7 +456,7 @@ parse_data(char *filepath, int *options, struct lyd_node *val_tree, const char *
     /* detect input format according to file suffix */
     informat = detect_data_format(filepath);
     if (informat == LYD_UNKNOWN) {
-        fprintf(stderr, "Unable to resolve format of the input file, please add \".xml\" or \".json\" suffix.\n");
+        fprintf(stderr, "Unable to resolve format of the input file, please add \".xml\", \".json\", or \".lyb\" suffix.\n");
         return EXIT_FAILURE;
     }
 
@@ -701,6 +703,8 @@ cmd_data(const char *arg)
                 outformat = LYD_XML;
             } else if (!strcmp(optarg, "json")) {
                 outformat = LYD_JSON;
+            } else if (!strcmp(optarg, "lyb")) {
+                outformat = LYD_LYB;
             } else {
                 fprintf(stderr, "Unknown output format \"%s\".\n", optarg);
                 goto cleanup;
