a78c92ca29c53741c0c0efad9873d4230e15fe98
Michal Vasko <mvasko@cesnet.cz>
2018-10-12 11:02:55 +0200

    parser BUGFIX handle DST time changes properly
    Fixes #623

--- src/parser.c
+++ src/parser.c
@@ -2435,6 +2435,9 @@ lyp_check_date(struct ly_ctx *ctx, const char *date)
     if (!r || r != &date[LY_REV_SIZE - 1]) {
         goto error;
     }
+    /* set some arbitrary non-0 value in case DST changes, it could move the day otherwise */
+    tm.tm_hour = 12;
+
     memcpy(&tm_, &tm, sizeof tm);
     mktime(&tm_); /* mktime modifies tm_ if it refers invalid date */
     if (tm.tm_mday != tm_.tm_mday) { /* e.g 2018-02-29 -> 2018-03-01 */
