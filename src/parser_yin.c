f824ad5c9440b11b688deb8e4529d5b859ef8367
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2018-10-14 23:58:15 +0200

    parser_yin PREPARE file and function header

---
+++ src/parser_yin.c
@@ -0,0 +1,15 @@
+/**
+ * @file parser_yin.c
+ * @author David Sedlák <xsedla1d@stud.fit.vutbr.cz>
+ * @brief YIN parser
+ */
+
+#include "common.h"
+#include "context.h"
+#include "libyang.h"
+
+LY_ERR
+parse_yin(struct ly_ctx *ctx, const char *data, struct lysp_module **mod_p)
+{
+
+}
