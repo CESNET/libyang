abbf357a2d51405a5ae2be59e0fd0dda57e7a697
Radek Krejci <rkrejci@cesnet.cz>
2018-11-26 13:26:37 +0100

    schema MAINTENANCE cleanup headers includes

--- src/tree_schema.c
+++ src/tree_schema.c
@@ -14,13 +14,10 @@
 
 #include "common.h"
 
-#include <ctype.h>
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <linux/limits.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
--- src/tree_schema_compile.c
+++ src/tree_schema_compile.c
@@ -15,15 +15,7 @@
 #include "common.h"
 
 #include <ctype.h>
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <linux/limits.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
 
 #include "libyang.h"
 #include "context.h"
