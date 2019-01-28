9b14f181d8494fbabbcfcc7de0dac967e70624cc
Martin Winter <mwinter@opensourcerouting.org>
2019-01-28 12:30:24 -0800

    lyb fix endian.h location for FreeBSD
    Signed-off-by: Martin Winter <mwinter@opensourcerouting.org>

--- src/parser_lyb.c
+++ src/parser_lyb.c
@@ -23,6 +23,8 @@
 # define le16toh(x) OSSwapLittleToHostInt16(x)
 # define le32toh(x) OSSwapLittleToHostInt32(x)
 # define le64toh(x) OSSwapLittleToHostInt64(x)
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+# include <sys/endian.h>
 #else
 # include <endian.h>
 #endif
--- src/printer_lyb.c
+++ src/printer_lyb.c
@@ -21,6 +21,8 @@
 #ifdef __APPLE__
 # include <libkern/OSByteOrder.h>
 # define htole64(x) OSSwapHostToLittleInt64(x)
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
+# include <sys/endian.h>
 #else
 # include <endian.h>
 #endif
