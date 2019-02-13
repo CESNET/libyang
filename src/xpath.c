60adc98903f01f3359e83610842d480ae14fb3af
Michal Vasko <mvasko@cesnet.cz>
2019-02-13 09:52:32 +0100

    xpath BUGFIX check maximum xpath string length
    Also, minor memory leaks fixed.
    Fixes #712

--- src/xpath.c
+++ src/xpath.c
@@ -316,17 +316,25 @@ print_set_debug(struct lyxp_set *set)
  * @param[in,out] str Pointer to the string to use.
  * @param[in,out] used Used bytes in \p str.
  * @param[in,out] size Allocated bytes in \p str.
+ *
+ * @return 0 on success, non-zero on error
  */
-static void
-cast_string_realloc(uint16_t needed, char **str, uint16_t *used, uint16_t *size)
+static int
+cast_string_realloc(struct ly_ctx *ctx, uint16_t needed, char **str, uint16_t *used, uint16_t *size)
 {
     if (*size - *used < needed) {
         do {
+            if ((UINT16_MAX - *size) < LYXP_STRING_CAST_SIZE_STEP) {
+                LOGERR(ctx, LY_EINVAL, "XPath string length limit (%u) reached.", UINT16_MAX);
+                return -1;
+            }
             *size += LYXP_STRING_CAST_SIZE_STEP;
         } while (*size - *used < needed);
         *str = ly_realloc(*str, *size * sizeof(char));
-        LY_CHECK_ERR_RETURN(!(*str), LOGMEM(NULL), );
+        LY_CHECK_ERR_RETURN(!(*str), LOGMEM(ctx), -1);
     }
+
+    return 0;
 }
 
 /**
@@ -340,7 +348,7 @@ cast_string_realloc(uint16_t needed, char **str, uint16_t *used, uint16_t *size)
  * @param[in,out] used Used bytes in \p str.
  * @param[in,out] size Allocated bytes in \p str.
  */
-static void
+static int
 cast_string_recursive(struct lyd_node *node, struct lys_module *local_mod, int fake_cont, enum lyxp_node_type root_type,
                       uint16_t indent, char **str, uint16_t *used, uint16_t *size)
 {
@@ -350,11 +358,13 @@ cast_string_recursive(struct lyd_node *node, struct lys_module *local_mod, int f
     struct lyd_node_anydata *any;
 
     if ((root_type == LYXP_NODE_ROOT_CONFIG) && (node->schema->flags & LYS_CONFIG_R)) {
-        return;
+        return 0;
     }
 
     if (fake_cont) {
-        cast_string_realloc(1, str, used, size);
+        if (cast_string_realloc(local_mod->ctx, 1, str, used, size)) {
+            return -1;
+        }
         strcpy(*str + (*used - 1), "\n");
         ++(*used);
 
@@ -366,12 +376,16 @@ cast_string_recursive(struct lyd_node *node, struct lys_module *local_mod, int f
     case LYS_LIST:
     case LYS_RPC:
     case LYS_NOTIF:
-        cast_string_realloc(1, str, used, size);
+        if (cast_string_realloc(local_mod->ctx, 1, str, used, size)) {
+            return -1;
+        }
         strcpy(*str + (*used - 1), "\n");
         ++(*used);
 
         LY_TREE_FOR(node->child, child) {
-            cast_string_recursive(child, local_mod, 0, root_type, indent + 1, str, used, size);
+            if (cast_string_recursive(child, local_mod, 0, root_type, indent + 1, str, used, size)) {
+                return -1;
+            }
         }
 
         break;
@@ -384,7 +398,9 @@ cast_string_recursive(struct lyd_node *node, struct lys_module *local_mod, int f
         }
 
         /* print indent */
-        cast_string_realloc(indent * 2 + strlen(value_str) + 1, str, used, size);
+        if (cast_string_realloc(local_mod->ctx, indent * 2 + strlen(value_str) + 1, str, used, size)) {
+            return -1;
+        }
         memset(*str + (*used - 1), ' ', indent * 2);
         *used += indent * 2;
 
@@ -405,28 +421,28 @@ cast_string_recursive(struct lyd_node *node, struct lys_module *local_mod, int f
         if (!(void*)any->value.tree) {
             /* no content */
             buf = strdup("");
-            LY_CHECK_ERR_RETURN(!buf, LOGMEM(local_mod->ctx), );
+            LY_CHECK_ERR_RETURN(!buf, LOGMEM(local_mod->ctx), -1);
         } else {
             switch (any->value_type) {
             case LYD_ANYDATA_CONSTSTRING:
             case LYD_ANYDATA_SXML:
             case LYD_ANYDATA_JSON:
                 buf = strdup(any->value.str);
-                LY_CHECK_ERR_RETURN(!buf, LOGMEM(local_mod->ctx), );
+                LY_CHECK_ERR_RETURN(!buf, LOGMEM(local_mod->ctx), -1);
                 break;
             case LYD_ANYDATA_DATATREE:
                 if (lyd_print_mem(&buf, any->value.tree, LYD_XML, LYP_WITHSIBLINGS)) {
-                    return;
+                    return -1;
                 }
                 break;
             case LYD_ANYDATA_XML:
                 if (lyxml_print_mem(&buf, any->value.xml, LYXML_PRINT_SIBLINGS)) {
-                    return;
+                    return -1;
                 }
                 break;
             case LYD_ANYDATA_LYB:
                 LOGERR(local_mod->ctx, LY_EINVAL, "Cannot convert LYB anydata into string.");
-                return;
+                return -1;
             case LYD_ANYDATA_STRING:
             case LYD_ANYDATA_SXMLD:
             case LYD_ANYDATA_JSOND:
@@ -439,7 +455,9 @@ cast_string_recursive(struct lyd_node *node, struct lys_module *local_mod, int f
 
         line = strtok_r(buf, "\n", &ptr);
         do {
-            cast_string_realloc(indent * 2 + strlen(line) + 1, str, used, size);
+            if (cast_string_realloc(local_mod->ctx, indent * 2 + strlen(line) + 1, str, used, size)) {
+                return -1;
+            }
             memset(*str + (*used - 1), ' ', indent * 2);
             *used += indent * 2;
 
@@ -455,16 +473,20 @@ cast_string_recursive(struct lyd_node *node, struct lys_module *local_mod, int f
 
     default:
         LOGINT(local_mod->ctx);
-        break;
+        return -1;
     }
 
     if (fake_cont) {
-        cast_string_realloc(1, str, used, size);
+        if (cast_string_realloc(local_mod->ctx, 1, str, used, size)) {
+            return -1;
+        }
         strcpy(*str + (*used - 1), "\n");
         ++(*used);
 
         --indent;
     }
+
+    return 0;
 }
 
 /**
@@ -488,11 +510,14 @@ cast_string_elem(struct lyd_node *node, struct lys_module *local_mod, int fake_c
     used = 1;
     size = LYXP_STRING_CAST_SIZE_START;
 
-    cast_string_recursive(node, local_mod, fake_cont, root_type, 0, &str, &used, &size);
+    if (cast_string_recursive(node, local_mod, fake_cont, root_type, 0, &str, &used, &size)) {
+        free(str);
+        return NULL;
+    }
 
     if (size > used) {
         str = ly_realloc(str, used * sizeof(char));
-        LY_CHECK_ERR_RETURN(!str, LOGMEM(local_mod->ctx), NULL);
+        LY_CHECK_ERR_RETURN(!str, free(str); LOGMEM(local_mod->ctx), NULL);
     }
     return str;
 }
@@ -8413,6 +8438,7 @@ lyxp_eval(const char *expr, const struct lyd_node *cur_node, enum lyxp_node_type
     }
     if ((rc == -1) && cur_node) {
         LOGPATH(ctx, LY_VLOG_LYD, cur_node);
+        lyxp_set_cast(set, LYXP_SET_EMPTY, cur_node, local_mod, options);
     }
 
 finish:
