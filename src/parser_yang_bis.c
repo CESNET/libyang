88bd6c548ba79bce176cd875e9b56e7e0ef4d8d4
Michal Vasko <mvasko@cesnet.cz>
2019-03-29 11:08:52 +0100

    yang parser BUGFIX double free
    Fixes #739

--- src/parser_yang_bis.c
+++ src/parser_yang_bis.c
@@ -9123,6 +9123,7 @@ void
 yyerror(YYLTYPE *yylloc, void *scanner, struct yang_parameter *param, ...)
 {
   free(*param->value);
+  *param->value = NULL;
   if (yylloc->first_line != -1) {
     if (*param->data_node && (*param->data_node) == (*param->actual_node)) {
       LOGVAL(param->module->ctx, LYE_INSTMT, LY_VLOG_LYS, *param->data_node, yyget_text(scanner));
