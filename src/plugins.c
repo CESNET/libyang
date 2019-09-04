4efd25f388883a761cfc61beb45bc979caed616c
Michal Vasko <mvasko@cesnet.cz>
2019-09-04 11:00:52 +0200

    plugins BUGFIX freeing union user type
    Fixes #868

--- src/plugins.c
+++ src/plugins.c
@@ -624,22 +624,15 @@ lytype_free(const struct lys_type *type, lyd_val value, const char *value_str)
     struct lyd_node_leaf_list leaf;
     struct lys_module *mod;
 
-    while (type->base == LY_TYPE_LEAFREF) {
-        type = &type->info.lref.target->type;
-    }
-
-    if (!type->der || !type->der->module) {
-        LOGINT(NULL);
-        return;
-    }
-
-    mod = type->der->module;
     memset(&sleaf, 0, sizeof sleaf);
     memset(&leaf, 0, sizeof leaf);
 
+    while (type->base == LY_TYPE_LEAFREF) {
+        type = &type->info.lref.target->type;
+    }
     if (type->base == LY_TYPE_UNION) {
         /* create a fake schema node */
-        sleaf.module = mod;
+        sleaf.module = type->parent->module;
         sleaf.name = "fake-leaf";
         sleaf.type = *type;
         sleaf.nodetype = LYS_LEAF;
@@ -652,9 +645,15 @@ lytype_free(const struct lys_type *type, lyd_val value, const char *value_str)
         /* find the original type */
         type = lyd_leaf_type(&leaf);
         if (!type) {
-            LOGINT(mod->ctx);
+            LOGINT(type->parent->module->ctx);
             return;
         }
+    }
+
+    mod = type->der->module;
+    if (!mod) {
+        LOGINT(type->parent->module->ctx);
+        return;
     }
 
     p = lytype_find(mod->name, mod->rev_size ? mod->rev[0].date : NULL, type->der->name);
