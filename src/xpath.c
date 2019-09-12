9409ef6cbf0c2ee6a68cf6e572b693506660d006
Michal Vasko <mvasko@cesnet.cz>
2019-09-12 11:47:17 +0200

    xpath CHANGE xml format forbidden for now

--- src/xpath.c
+++ src/xpath.c
@@ -4086,6 +4086,8 @@ xpath_name(struct lyxp_set **args, uint16_t arg_count, struct lyxp_set *set, int
         case LYD_JSON:
             rc = asprintf(&str, "%s:%s", mod->name, name);
             break;
+        default:
+            LOGINT_RET(set->ctx);
         }
         LY_CHECK_ERR_RET(rc == -1, LOGMEM(set->ctx), LY_EMEM);
         set_fill_string(set, str, strlen(str));
@@ -5153,6 +5155,9 @@ moveto_resolve_model(const char *prefix, uint16_t len, LYD_FORMAT format, struct
         mod = ly_ctx_get_module(ctx, str, NULL);
         free(str);
         break;
+    default:
+        LOGINT(ctx);
+        return NULL;
     }
 
     if (!mod->implemented) {
