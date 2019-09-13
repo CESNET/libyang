8859ab9e528257d84a04749a0de1a766e152b2fd
David Sedlák <xsedla1d@stud.fit.vutbr.cz>
2019-09-13 10:27:29 +0200

    yin parser CHANGE remove finished TODO comment from code

--- src/parser_yin.c
+++ src/parser_yin.c
@@ -3410,7 +3410,6 @@ yin_parse_element_generic(struct yin_parser_ctx *ctx, const char *name, size_t n
     //(*element)->stmt = lydict_insert(ctx->xml_ctx.ctx, prefix ? prefix : name, prefix_len ? prefix_len + name_len + 1 : name_len);
     (*element)->stmt = name2nsname(ctx, name, name_len, prefix, prefix_len);
     LY_CHECK_ERR_GOTO(!(*element)->stmt, ret = LY_EMEM, cleanup);
-    /* TODO map prefix to module name */
 
     (*element)->kw = yin_match_keyword(ctx, name, name_len, prefix, prefix_len, parent);
 
