c10f4dfca8be9a47a4807a4f5a0bb6244764aa88
Michal Vasko <mvasko@cesnet.cz>
2019-11-13 08:24:40 +0100

    yin parser BUGFIX properly clean up after must parsing failure
    Fixes #941

--- src/parser_yin.c
+++ src/parser_yin.c
@@ -1919,18 +1919,24 @@ error:
 static int
 fill_yin_must(struct lys_module *module, struct lyxml_elem *yin, struct lys_restr *must, struct unres_schema *unres)
 {
+    int ret = EXIT_FAILURE;
     const char *value;
 
+    must->expr = NULL;
     GETVAL(module->ctx, value, yin, "condition");
     must->expr = transform_schema2json(module, value);
     if (!must->expr) {
         goto error;
     }
 
-    return read_restr_substmt(module, must, yin, unres);
+    ret = read_restr_substmt(module, must, yin, unres);
 
 error:
-    return EXIT_FAILURE;
+    if (ret) {
+        lydict_remove(module->ctx, must->expr);
+        must->expr = NULL;
+    }
+    return ret;
 }
 
 static int
