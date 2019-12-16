294b76d6394eadd35b1b1f6e15244e8f8b8fad81
Michal Vasko <mvasko@cesnet.cz>
2019-12-16 10:37:54 +0100

    schema BUGFIX return value check

--- src/tree_schema_helpers.c
+++ src/tree_schema_helpers.c
@@ -92,7 +92,8 @@ lys_resolve_schema_nodeid(struct lysc_ctx *ctx, const char *nodeid, size_t nodei
         }
         if (implement && !mod->implemented) {
             /* make the module implemented */
-            lys_set_implemented_internal((struct lys_module*)mod, 2);
+            ret = lys_set_implemented_internal((struct lys_module*)mod, 2);
+            LY_CHECK_RET(ret);
         }
         if (context_node && context_node->nodetype == LYS_ACTION) {
             /* move through input/output manually */
