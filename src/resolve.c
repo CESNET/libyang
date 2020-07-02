6130b11f7bd4a1100badd93bcf599b484ca06577
Michal Vasko <mvasko@cesnet.cz>
2020-07-02 09:28:41 +0200

    resolve BUGFIX implement modules after augments
    Fixes #1130

--- src/resolve.c
+++ src/resolve.c
@@ -7429,14 +7429,19 @@ resolve_unres_schema(struct lys_module *mod, struct unres_schema *unres)
      * if-features are resolved here to make sure that we will have all if-features for
      * later check of feature circular dependency */
     if (resolve_unres_schema_types(unres, UNRES_USES | UNRES_IFFEAT | UNRES_TYPE_DER | UNRES_TYPE_DER_TPDF | UNRES_TYPE_DER_TPDF
-                                   | UNRES_TYPE_LEAFREF | UNRES_MOD_IMPLEMENT | UNRES_AUGMENT | UNRES_CHOICE_DFLT | UNRES_IDENT,
+                                   | UNRES_AUGMENT | UNRES_CHOICE_DFLT | UNRES_IDENT,
                                    mod->ctx, 1, 0, &resolved)) {
+        return -1;
+    }
+
+    if (resolve_unres_schema_types(unres, UNRES_MOD_IMPLEMENT, mod->ctx, 1, 0, &resolved)) {
         return -1;
     }
 
     /* another batch of resolved items */
     if (resolve_unres_schema_types(unres, UNRES_TYPE_IDENTREF | UNRES_FEATURE | UNRES_TYPEDEF_DFLT | UNRES_TYPE_DFLT
-                                   | UNRES_LIST_KEYS | UNRES_LIST_UNIQ | UNRES_EXT, mod->ctx, 1, 0, &resolved)) {
+                                   | UNRES_TYPE_LEAFREF | UNRES_LIST_KEYS | UNRES_LIST_UNIQ | UNRES_EXT, mod->ctx, 1, 0,
+                                   &resolved)) {
         return -1;
     }
 
