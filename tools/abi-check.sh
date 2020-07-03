13a92500d5f5b2e0448bffb529b93b093cb9f618
Ondřej Surý <Ondej Sur ondrej@debian.org>
2020-07-03 15:25:19 +0200

    Install and use universal-ctags snaps for building the ABI dumps

--- tools/abi-check.sh
+++ tools/abi-check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 status=0
 # Dump the current ABI
-abi-dumper ./build/libyang.so -o ./build/libyang.dump -lver "$(PKG_CONFIG_PATH=./build pkg-config --modversion libyang)"
+LC_ALL=C.UTF-8 PATH=/snap/bin:$PATH abi-dumper ./build/libyang.so -o ./build/libyang.dump -lver "$(PKG_CONFIG_PATH=./build pkg-config --modversion libyang)" -public-headers ./src -public-headers ./build/src
 # Compare the current ABI with previous ABI
 abi-compliance-checker -l libyang.so -old ./libyang.dump -new ./build/libyang.dump -s || status=$?
 # Generate and dump text output
