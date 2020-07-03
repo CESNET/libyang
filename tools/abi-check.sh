07b1ab7a96c9a92ed4787fd4065378ea45418490
Ondřej Surý <Ondej Sur ondrej@debian.org>
2020-07-03 13:08:10 +0200

    Generate and dump text output from the ABI Compliance Checker HTML output

--- tools/abi-check.sh
+++ tools/abi-check.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
+status=0
 # Dump the current ABI
 abi-dumper ./build/libyang.so -o ./build/libyang.dump -lver "$(PKG_CONFIG_PATH=./build pkg-config --modversion libyang)"
 # Compare the current ABI with previous ABI
-abi-compliance-checker -l libyang.so -old ./libyang.dump -new ./build/libyang.dump -s
+abi-compliance-checker -l libyang.so -old ./libyang.dump -new ./build/libyang.dump -s || status=$?
+# Generate and dump text output
+w3m -dump -O ascii -T text/html "$(find "compat_reports/${SONAME}" -name '*.html')"
+exit $status
