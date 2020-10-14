997f982a325ac6f444385b6d8ccaea225bdd8568
Michal Vasko <mvasko@cesnet.cz>
2020-10-14 13:09:05 +0200

    plugins types BUGFIX check module is implemented using the flag
    The compiled module may temporarily not exist
    because it is being recompiled.

--- tests/utests/schema/test_tree_schema_compile.c
+++ tests/utests/schema/test_tree_schema_compile.c
@@ -2280,7 +2280,7 @@ test_uses(void **state)
     assert_int_equal(1, LY_ARRAY_COUNT(child->iffeatures[0].features));
     assert_string_equal("f", child->iffeatures[0].features[0]->name);
     assert_int_equal(LY_EINVAL, lys_feature_enable(mod->parsed->imports[0].module, "f"));
-    logbuf_assert("Module \"grp\" is not implemented so all its features are permanently disabled without a chance to change it.");
+    logbuf_assert("Module \"grp\" is not implemented so all its features are permanently disabled.");
     assert_int_equal(LY_ENOT, lysc_iffeature_value(&child->iffeatures[0]));
 
     /* make the imported module implemented and enable the feature */
