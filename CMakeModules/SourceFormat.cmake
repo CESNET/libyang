5348e6ce37afa1ff9dbdfbf9b20b457f84202505
Radek Krejci <rkrejci@cesnet.cz>
2021-04-13 21:18:51 +0200

    tests FEATURE check source code formatting as one of the tests

--- CMakeModules/SourceFormat.cmake
+++ CMakeModules/SourceFormat.cmake
@@ -1,12 +1,22 @@
 # format source files with uncrustify
+
+# check that format checking is available - always use before SOURCE_FORMAT
+macro(SOURCE_FORMAT_ENABLE)
+    find_package(Uncrustify 0.71)
+    if(UNCRUSTIFY_FOUND)
+        set(SOURCE_FORMAT_ENABLED TRUE)
+    else()
+        set(SOURCE_FORMAT_ENABLED FALSE)
+    endif()
+endmacro()
+
 # files are expected to be a list and relative paths are resolved wtih respect to CMAKE_SOURCE DIR
 macro(SOURCE_FORMAT)
     if(NOT ${ARGC})
         message(FATAL_ERROR "source_format() needs a list of files to format!")
     endif()
 
-    find_package(Uncrustify 0.71)
-    if(UNCRUSTIFY_FOUND)
+    if(SOURCE_FORMAT_ENABLED)
         add_custom_target(format
                 COMMAND ${UNCRUSTIFY} -c ${CMAKE_SOURCE_DIR}/uncrustify.cfg --no-backup --replace ${ARGN}
                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
@@ -16,5 +26,7 @@ macro(SOURCE_FORMAT)
                 COMMAND ${UNCRUSTIFY} -c ${CMAKE_SOURCE_DIR}/uncrustify.cfg --check ${ARGN}
                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
                 COMMENT "Checking format of the sources with ${UNCRUSTIFY} ...")
+
+        set(SOURCE_FORMAT_ENABLED TRUE)
     endif()
 endmacro()
