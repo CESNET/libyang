350a6bf69e03d19fe996cba992b49556ae2ce8ab
aPiecek <piecek@cesnet.cz>
2021-05-25 07:59:10 +0200

    json BUGFIX in lyjson_number
    The minus sign was taken as part of the number, which is not correct
    in this expression.

--- src/json.c
+++ src/json.c
@@ -428,7 +428,7 @@ invalid_character:
                 /* adding zeros at the end */
                 num_len = exponent + e_val;
                 dp_position = num_len; /* decimal point is behind the actual value */
-            } else if ((size_t)labs(e_val) < exponent) {
+            } else if ((size_t)labs(e_val) < (exponent - minus)) {
                 /* adding decimal point between the integer's digits */
                 num_len = exponent + 1;
                 dp_position = exponent + e_val;
