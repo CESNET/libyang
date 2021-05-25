3b62f3b7b4191ccd714c948a3592c2720a87b059
Michal Vasko <mvasko@cesnet.cz>
2021-05-25 08:53:38 +0200

    build REFACTOR doc module improvements
    Also, Doxyfile.in is now generic for any
    project.

--- CMakeModules/GenDoc.cmake
+++ CMakeModules/GenDoc.cmake
@@ -1,5 +1,5 @@
 # Prepare building doxygen documentation
-macro(LIBYANG_DOC)
+macro(GEN_DOC INPUT_FILES PROJECT_VERSION PROJECT_DESCRIPTION DOC_LOGO)
     find_package(Doxygen)
     if(DOXYGEN_FOUND)
         find_program(DOT_PATH dot PATH_SUFFIXES graphviz2.38/bin graphviz/bin)
@@ -9,10 +9,20 @@ macro(LIBYANG_DOC)
             set(HAVE_DOT "NO")
             message(AUTHOR_WARNING "Doxygen: to generate UML diagrams please install graphviz")
         endif()
+
+        # target doc
         add_custom_target(doc
                 COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/Doxyfile
                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-        string(REPLACE ";" " " DOXY_HEADERS "${headers};${PROJECT_BINARY_DIR}/src/version.h;${type_plugins}")
+
+        # generate list with spaces as separators
+        string(REPLACE ";" " " DOXY_INPUT "${INPUT_FILES}")
+
+        # make other arguments into variables
+        set(PROJECT_VERSION ${PROJECT_VERSION})
+        set(PROJECT_DESCRIPTION ${PROJECT_DESCRIPTION})
+        set(DOC_LOGO ${DOC_LOGO})
+
         configure_file(Doxyfile.in Doxyfile)
     endif()
 endmacro()
