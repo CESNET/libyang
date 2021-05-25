938a3a5af5726145245bea3556c998e6b1c3264d
Michal Vasko <mvasko@cesnet.cz>
2021-05-25 09:02:52 +0200

    compat REFACTOR put header check script with compat

--- compat/check_includes.sh
+++ compat/check_includes.sh
@@ -28,13 +28,16 @@ check_compat_macro () {
 	done
 }
 
+check_compat_func vdprintf
 check_compat_func asprintf
-check_compat_func get_current_dir_name
+check_compat_func vasprintf
 check_compat_func getline
 check_compat_func strndup
 check_compat_func strnstr
-check_compat_func vasprintf
-check_compat_func vdprintf
+check_compat_func strdupa
+check_compat_func strchrnul
+check_compat_func get_current_dir_name
+check_compat_func pthread_mutex_timedlock
 check_compat_func UNUSED
 check_compat_macro _PACKED
 
