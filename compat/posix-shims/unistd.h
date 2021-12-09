54183b7babb95711e34a00f51829b3d39bf70da3
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-12-09 22:31:18 +0100

    compat: <unistd.h>: make X_OK a no-op on Windows
    There's nothing to check via access() on Windows for directory
    permissions, and I have no clue if the underlying _access() ignores
    unknown mode flags, so let's play it safe. The worst case is that
    access() will be too optimistic, which wouldn't really hart because of
    TOCTOU races, anyway. In other words, if anything breaks by this patch,
    then it's broken already.

--- compat/posix-shims/unistd.h
+++ compat/posix-shims/unistd.h
@@ -18,7 +18,7 @@
    These may be OR'd together.  */
 #define R_OK    4       /* Test for read permission.  */
 #define W_OK    2       /* Test for write permission.  */
-//#define   X_OK    1       /* execute permission - unsupported in windows*/
+#define X_OK    0       /* jkt: unsupported on Windows, so we don't really care */
 #define F_OK    0       /* Test for existence.  */
 
 #define access _access
