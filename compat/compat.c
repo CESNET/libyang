c8805306689e77b70250986363bf1c180d1f37bb
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-12-11 23:40:26 +0100

    MSVC: compat: implement setenv()

--- compat/compat.c
+++ compat/compat.c
@@ -304,3 +304,25 @@ dirname(char *path)
 #error No dirname() implementation for this platform is available.
 #endif
 #endif
+
+#ifndef HAVE_SETENV
+#ifdef _WIN32
+int
+setenv(const char *name, const char *value, int overwrite)
+{
+    int errcode = 0;
+
+    if (!overwrite) {
+        size_t envsize = 0;
+        errcode = getenv_s(&envsize, NULL, 0, name);
+        if (errcode || envsize) {
+            return errcode;
+        }
+    }
+    return _putenv_s(name, value);
+}
+
+#else
+#error No setenv() implementation for this platform is available.
+#endif
+#endif
