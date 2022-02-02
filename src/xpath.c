24bd22f5e40f37fb6ae796bb8eebc9de58bae00e
Michal Vasko <mvasko@cesnet.cz>
2022-02-02 11:16:56 +0100

    xpath BUGFIX evaluation of * on schema
    All modules of the nodes must match.
    Refs #1780

--- src/xpath.c
+++ src/xpath.c
@@ -5472,7 +5472,7 @@ static LY_ERR
 moveto_scnode_check(const struct lysc_node *node, const struct lysc_node *ctx_scnode, const struct lyxp_set *set,
         const char *node_name, const struct lys_module *moveto_mod)
 {
-    if (!moveto_mod && (!node_name || strcmp(node_name, "*"))) {
+    if (!moveto_mod && node_name && strcmp(node_name, "*")) {
         switch (set->format) {
         case LY_VALUE_SCHEMA:
         case LY_VALUE_SCHEMA_RESOLVED:
