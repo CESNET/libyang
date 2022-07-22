03fe620abab66176b2929c46ee0e537f615d004e
Michal Vasko <mvasko@cesnet.cz>
2022-07-22 16:23:13 +0200

    yanglint REFACTOR put brackets outside macros

--- tools/lint/main_ni.c
+++ tools/lint/main_ni.c
@@ -518,10 +518,11 @@ fill_context(int argc, char *argv[], struct context *c)
 
     opterr = 0;
 #ifndef NDEBUG
-    while ((opt = getopt_long(argc, argv, "hvVQf:p:DF:iP:qs:net:d:lL:o:O:R:myY:G:", options, &opt_index)) != -1) {
+    while ((opt = getopt_long(argc, argv, "hvVQf:p:DF:iP:qs:net:d:lL:o:O:R:myY:G:", options, &opt_index)) != -1)
 #else
-    while ((opt = getopt_long(argc, argv, "hvVQf:p:DF:iP:qs:net:d:lL:o:O:R:myY:", options, &opt_index)) != -1) {
+    while ((opt = getopt_long(argc, argv, "hvVQf:p:DF:iP:qs:net:d:lL:o:O:R:myY:", options, &opt_index)) != -1)
 #endif
+    {
         switch (opt) {
         case 'h': /* --help */
             help(0);
