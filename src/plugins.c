f85bfcc6a4eb60981117da133848c85d6773aaeb
Dwade <jchedewei@163.com.com>
2022-10-26 15:34:43 +0800

    resolve Segmentation fault when get extensions plugin

--- src/plugins.c
+++ src/plugins.c
@@ -458,7 +458,7 @@ ext_get_plugin(const char *name, const char *module, const char *revision)
 
     for (u = 0; u < ext_plugins_count; u++) {
         if (!strcmp(name, ext_plugins[u].name) && !strcmp(module, ext_plugins[u].module) &&
-                ((!revision && !ext_plugins[u].revision) || (revision && !strcmp(revision, ext_plugins[u].revision)))) {
+                (!ext_plugins[u].revision || (revision && !strcmp(revision, ext_plugins[u].revision)))) {
             /* we have the match */
             return ext_plugins[u].plugin;
         }
