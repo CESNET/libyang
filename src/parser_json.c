1241ebc2575bf5648338a55f9e2e7e00e4c74adc
Michal Vasko <mvasko@cesnet.cz>
2023-04-04 10:03:45 +0200

    parser json BUGFIX proper skip of invalid data

--- src/parser_json.c
+++ src/parser_json.c
@@ -1558,6 +1558,8 @@ lydjson_subtree_r(struct lyd_json_ctx *lydctx, struct lyd_node *parent, struct l
             if ((r = lydjson_data_skip(lydctx->jsonctx))) {
                 rc = r;
             }
+            r = lyjson_ctx_next(lydctx->jsonctx, &status);
+            LY_CHECK_ERR_GOTO(r, rc = r, cleanup);
             goto cleanup;
         } else if (r) {
             /* error */
