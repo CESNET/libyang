CESNET/libyang

commit 183b911ec920f74a88476cf1e90f9d96ff4c509d

printer json BUGFIX union realtype value print
Fixes #1729

src/printer_json.c
@@ -333,10 +333,19 @@ static LY_ERR
 json_print_value(struct jsonpr_ctx *ctx, const struct lyd_value *val)
 {
     ly_bool dynamic;
+    LY_DATA_TYPE basetype;
     const char *value = val->realtype->plugin->print(ctx->ctx, val, LY_VALUE_JSON, NULL, &dynamic, NULL);
 
+    basetype = val->realtype->basetype;
+
+print_val:
     /* leafref is not supported */
-    switch (val->realtype->basetype) {
+    switch (basetype) {
+    case LY_TYPE_UNION:
+        /* use the resolved type */
+        basetype = val->subvalue->value.realtype->basetype;
+        goto print_val;
+
     case LY_TYPE_BINARY:
     case LY_TYPE_STRING:
     case LY_TYPE_BITS:
@@ -346,7 +355,6 @@ json_print_value(struct jsonpr_ctx *ctx, const struct lyd_value *val)
     case LY_TYPE_UINT64:
     case LY_TYPE_DEC64:
     case LY_TYPE_IDENT:
-    case LY_TYPE_UNION:
         json_print_string(ctx->out, value);
         break;
 

commit 0b46b650130d5eb46541345b067428d2ed62b247

printer json BUGFIX nested union value print
Fix an infinite loop in json_print_value() when
resolved type of a union member is also union.

Fixes: 183b911ec920 ("printer json BUGFIX union realtype value print")

Signed-off-by: Ilyes Ben Hamouda <ilyes.ben_hamouda@6wind.com>

src/printer_json.c
@@ -363,7 +363,8 @@ print_val:
     switch (basetype) {
     case LY_TYPE_UNION:
         /* use the resolved type */
-        basetype = val->subvalue->value.realtype->basetype;
+        val = &val->subvalue->value;
+        basetype = val->realtype->basetype;
         goto print_val;
 
     case LY_TYPE_BINARY: