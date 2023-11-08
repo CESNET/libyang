d05e3929e1df86c038d079e71340f3caa2d7c437
aPiecek <piecek@cesnet.cz>
2023-11-08 13:44:07 +0100

    plugins types FEATURE sort callback in bits

--- src/plugins_types.h
+++ src/plugins_types.h
@@ -744,6 +744,12 @@ LIBYANG_API_DECL LY_ERR lyplg_type_compare_bits(const struct ly_ctx *ctx, const 
         const struct lyd_value *val2);
 
 /**
+ * @brief Implementation of the ::lyplg_type_sort_clb for the built-in bits type.
+ */
+LIBYANG_API_DEF int lyplg_type_sort_bits(const struct ly_ctx *ctx, const struct lyd_value *val1,
+        const struct lyd_value *val2);
+
+/**
  * @brief Implementation of the ::lyplg_type_print_clb for the built-in bits type.
  */
 LIBYANG_API_DECL const void *lyplg_type_print_bits(const struct ly_ctx *ctx, const struct lyd_value *value,
