CESNET/libyang

commit c80fcc9d0d0c38af26f70e38075e8c11c9f5391e

diff BUGFIX diff merge userord node order
Fixes #3316

src/diff.c
@@ -1651,14 +1651,18 @@ lyd_diff_merge_create(struct lyd_node **diff_match, struct lyd_node **diff, enum
             orig_meta = lyd_find_meta((*diff_match)->meta, NULL, orig_meta_name);
             LY_CHECK_ERR_RET(!orig_meta, LOGERR_META(ctx, orig_meta_name, *diff_match), LY_EINVAL);
 
-            /* the (incorrect) assumption made here is that there are no previous diff nodes that would affect
-             * the anchors stored in the metadata */
             if (strcmp(lyd_get_meta_value(meta), lyd_get_meta_value(orig_meta))) {
                 /* deleted + created at another position -> operation REPLACE */
                 LY_CHECK_RET(lyd_diff_change_op(*diff_match, LYD_DIFF_OP_REPLACE));
 
                 /* add anchor metadata */
                 LY_CHECK_RET(lyd_dup_meta_single(meta, *diff_match, NULL));
+
+                /* previous created nodes affect the metadata so move it at the end */
+                child = lyd_first_sibling(*diff_match);
+                if (child->next) {
+                    LY_CHECK_RET(lyd_insert_after(child->prev, *diff_match));
+                }
             } else {
                 /* deleted + created at the same position -> operation NONE */
                 LY_CHECK_RET(lyd_diff_change_op(*diff_match, LYD_DIFF_OP_NONE));

tests/utests/data/test_diff.c
@@ -976,12 +976,12 @@ test_userord_list(void **state)
             "    <l1>a</l1>\n"
             "    <l2 yang:operation=\"delete\">1</l2>\n"
             "  </ul>\n"
-            "  <ul yang:orig-key=\"[l1='a']\" yang:operation=\"replace\" yang:key=\"[l1='c']\">\n"
-            "    <l1>b</l1>\n"
-            "  </ul>\n"
             "  <ul yang:operation=\"none\">\n"
             "    <l1>c</l1>\n"
             "    <l2 yang:operation=\"replace\" yang:orig-default=\"false\" yang:orig-value=\"3\">33</l2>\n"
+            "  </ul>\n"
+            "  <ul yang:orig-key=\"[l1='a']\" yang:operation=\"replace\" yang:key=\"[l1='c']\">\n"
+            "    <l1>b</l1>\n"
             "  </ul>\n"
             "</df>\n";
 

commit 9578d7a7fba9310ac7a8079af0cdd160bc080b87

diff BUGFIX check nodes before inserting after
The mode must be different and have the same schema.

Fixes: c80fcc9d0d0c ("diff BUGFIX diff merge userord node order")

src/diff.c
@@ -1660,7 +1660,8 @@ lyd_diff_merge_create(struct lyd_node **diff_match, struct lyd_node **diff, enum
 
                 /* previous created nodes affect the metadata so move it at the end */
                 child = lyd_first_sibling(*diff_match);
-                if (child->next) {
+                if (child->next && (child->prev != *diff_match) &&
+                        (child->prev->schema == (*diff_match)->schema)) {
                     LY_CHECK_RET(lyd_insert_after(child->prev, *diff_match));
                 }
             } else {