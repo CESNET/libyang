2ce01757e4473f6d5bbd35b1036bbd893a75c349
roman <Roman.Janota@cesnet.cz>
2025-06-03 11:02:55 +0200

    schema mount REFACTOR move api decl to plugins_ext

--- src/plugins_exts.h
+++ src/plugins_exts.h
@@ -1140,6 +1140,46 @@ LIBYANG_API_DECL LY_ERR lyplg_ext_schema_mount_get_parent_ref(const struct lysc_
 LIBYANG_API_DECL LY_ERR lyplg_ext_schema_mount_create_context(const struct lysc_ext_instance *ext,
         const struct lyd_node *parent, struct ly_ctx **ctx);
 
+/**
+ * @brief Create a shared schema mount context for a schema mount point.
+ *
+ * Does nothing if the mount point is not shared or if the context already exists.
+ *
+ * For printed contexts (read-only), all shared contexts must be created beforehand
+ * by calling this function as they cannot be created once the context is printed.
+ *
+ * @param[in] ext Compiled extension instance of a schema mount point.
+ * @param[in] ext_data ietf-yang-schema-mount and ietf-yang-library YANG data for the @p ext mount point.
+ * @return LY_ENOT if @p ext mount point data was not found in @p ext_data, other LY_ERR value otherwise.
+*/
+LIBYANG_API_DECL LY_ERR lyplg_ext_schema_mount_create_shared_context(struct lysc_ext_instance *ext,
+        const struct lyd_node *ext_data);
+
+/**
+ * @brief Destroy all the shared schema mount contexts for a given libyang context.
+ *
+ * For standard contexts this is done automatically when the context is destroyed.
+ *
+ * For printed contexts (read-only), all shared contexts must be destroyed
+ * by calling this function as otherwise they will be lost once the original context is destroyed.
+ *
+ * @param[in] ext Compiled extension instance of a schema mount point. All the shared schema mount
+ * contexts that belong to the same libyang context as @p ext will be destroyed.
+ */
+LIBYANG_API_DECL void lyplg_ext_schema_mount_destroy_shared_contexts(struct lysc_ext_instance *ext);
+
+/**
+ * @brief Destroy all the inline contexts for a given libyang context.
+ *
+ * For standard contexts this is done automatically when the context is destroyed.
+ *
+ * Inline contexts are not supported in printed contexts.
+ *
+ * @param[in] ext Compiled extension instance of a schema mount point. All the inline schema mount
+ * contexts that belong to the same libyang context as @p ext will be destroyed.
+ */
+LIBYANG_API_DECL void lyplg_ext_schema_mount_destroy_inline_contexts(struct lysc_ext_instance *ext);
+
 /** @} pluginsExtensions */
 
 #ifdef __cplusplus
