c3e86e15ea3a964a3afe8180c951ac7d0b445621
roman <Roman.Janota@cesnet.cz>
2025-07-01 11:23:46 +0200

    libyang REFACTOR remove leftover include

--- src/libyang.h
+++ src/libyang.h
@@ -34,7 +34,6 @@ extern "C" {
 #include "parser_schema.h"
 #include "printer_data.h"
 #include "printer_schema.h"
-#include "schema_mount.h"
 #include "set.h"
 #include "tree.h"
 #include "tree_data.h"
