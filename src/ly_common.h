3e3238f6933fde5b6897cbbb4bae2aa5d5996f65
roman <Roman.Janota@cesnet.cz>
2025-07-07 13:30:54 +0200

    schema mount UPDATE do not create redundant ctxs

--- src/ly_common.h
+++ src/ly_common.h
@@ -817,9 +817,11 @@ LY_ERR ly_strcat(char **dest, const char *format, ...) _FORMAT_PRINTF(2, 3);
  * @brief Get schema (context) for a mount point.
  *
  * @param[in] ext Compiled extension instance where the context is also stored.
+ * @param[in] parent Data parent node instance of a schema node with @p ext instance. In special cases
+ * (when not working with data) it can be NULL!
  * @param[out] ext_ctx Schema to use for parsing the data.
  * @return LY_ERR value.
  */
-LY_ERR lyplg_ext_schema_mount_get_ctx(struct lysc_ext_instance *ext, const struct ly_ctx **ext_ctx);
+LY_ERR lyplg_ext_schema_mount_get_ctx(struct lysc_ext_instance *ext, const struct lyd_node *parent, const struct ly_ctx **ext_ctx);
 
 #endif /* LY_COMMON_H_ */
