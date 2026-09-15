be1e79e5d8406f5c9cde3170ab5d967e2ea5503d
Michal Vasko <mvasko@cesnet.cz>
2026-09-15 14:42:10 +0200

    validation BUGFIX proper ext data validation
    Refs #2548

--- src/validation.c
+++ src/validation.c
@@ -1872,7 +1872,7 @@ next_iter:
     LY_VAL_ERR_GOTO(r, rc = r, val_opts, cleanup);
 
     LY_LIST_FOR(first, node) {
-        if ((node->flags & LYD_EXT) || !node->schema || (!node->parent && mod && (lyd_owner_module(node) != mod))) {
+        if (((node->flags & LYD_EXT) && !ext) || !node->schema || (!node->parent && mod && (lyd_owner_module(node) != mod))) {
             /* condensed condition of the previous loop */
             break;
         }
